24eb2e322806a6ac99e902c9f63a93cc177b765b
Vladimir Ershov <secamm66@mail.ru>
2024-09-05 00:20:11 +0300

    Add dependency_bom

---
+++ dependency_bom/src/main/java/ru/ershov/project/dependency_bom/DependencyBomApplication.java
@@ -0,0 +1,13 @@
+package ru.ershov.project.dependency_bom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyBomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyBomApplication.class, args);
+    }
+
+}
